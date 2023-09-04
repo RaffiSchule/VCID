4840ad9582a4d0f1b6ecfeee15b89c7cc1c5c487
RaffiSchule <raffael.engelmann@student.ipso.ch>
2023-09-04 08:52:56 +0200

    Publish all scripts and config files

---
+++ Infrastruktur_Scripts/01_Install_Docker_DockerCompose.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package index and install required dependencies
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+# Install Docker
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sudo sh get-docker.sh
+
+# Start and enable Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Install Docker Compose
+sudo apt install docker-compose-plugin
+sudo apt-get install docker-compose-plugin
+
+# Add your user to the docker group (optional, allows running Docker without sudo)
+sudo usermod -aG docker $ubuntu
+# Maybe restart the system (sudo shutdown -r now) to get the updated rights
+
+# Verify Docker and Docker Compose installation
+docker --version
+docker-compose --version
+echo "Docker and Docker Compose installation completed."
---
+++ Infrastruktur_Scripts/02_Install_App_Image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Change directory
+cd /home
+sudo mkdir praxisarbeit
+cd praxisarbeit
+
+# Download microblog image
+sudo wget https://os.s1.scloud.switch.ch/swift/v1/ifa-demo/microblog-13-lb-mariadb-V2.zip
+
+# Unzip image
+sudo apt install unzip
+sudo unzip microblog-13-lb-mariadb-V2.zip
+
+# Use Docker Compose to start the app
+docker-compose up -d
+
+
